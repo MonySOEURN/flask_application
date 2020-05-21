e3e2beb41e0fef541243749cd366ae67f69e0cce
Mony SOEURN <soeurn.mony@gmail.com>
2020-05-21 22:04:20 +0700

    [TASK] API update user account

--- app.py
+++ app.py
@@ -47,14 +47,11 @@ def users():
 @app.route('/users/<id>')
 def user(id):
     user = mongo.db.users.find_one({'_id': ObjectId(id)})
-    if user == 'null':
+    if user is not None:
         resp = dumps({ 'message': "user retrieved successfully.", 'status': 200, 'data': user})
+        return resp
     else :
-        resp = jsonify({
-            'message': "user not found",
-            'status': 404
-        })
-    return resp
+        return user_notFound()
 
 # delete user account
 @app.route('/users/delete/<id>', methods = ['DELETE'])
@@ -67,12 +64,52 @@ def user_delete(id):
     resp.status_code = 200
     return resp
 
+@app.route('/users/update/<id>', methods = ['PUT'])
+def user_update(id):
+    _id = id
+    _json = request.json
+    _name = _json['name']
+    _email = _json['email']
+    _password = _json['pwd']
+
+    if _name and _email and _password and request.method == 'PUT':
+        _hashed_password = generate_password_hash(_password)
+        mongo.db.users.update_one({
+                '_id': ObjectId(_id['$oid'])
+                if '$oid' in _id else ObjectId(_id)
+            },
+            {
+                '$set': {
+                    'name' : _name,
+                    'email' : _email,
+                    'pwd' : _hashed_password
+                }
+            }
+        )
+        resp = jsonify({
+            'message': "user update successfully.",
+            'status': 200
+        })
+        resp.status_code = 200
+        return resp
+    else :
+        return user_notFound()
+
 # error url response
 @app.errorhandler(404)
 def not_found(error=None):
     message = {
         'status': 404,
         'message': 'Not Found' + request.url 
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+
+def user_notFound(error=None):
+    message = {
+        'message': "user not found",
+        'status': 404
     }
     resp = jsonify(message)
     resp.status_code = 404
