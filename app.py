2280e21c5f178178dd273f004e4b1a4c6a2fd71a
Mony SOEURN <soeurn.mony@gmail.com>
2020-05-21 21:04:37 +0700

    [TASK] API create user

--- app.py
+++ app.py
@@ -12,6 +12,40 @@ app.config['MONGO_URI'] = "mongodb://localhost:27017/Users"
 
 mongo = PyMongo(app)
 
+# create new user account
+@app.route('/add', methods = ['POST'])
+def add_user():
+    _json = request.json
+    _name = _json['name']
+    _email = _json['email']
+    _password = _json['pwd']
+
+    if _name and _email and _password and request.method == 'POST':
+        _hashed_password = generate_password_hash(_password)
+
+        id = mongo.db.users.insert({
+            'name' : _name,
+            'email' : _email,
+            'pwd' : _hashed_password
+        })
+        resp = jsonify("user added successfully")
+
+        resp.status_code = 200
+        return resp
+    else :
+        return not_found()
+
+# error url response
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found' + request.url 
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+
 if __name__ == "__main__":
     app.run(debug=True)
 
