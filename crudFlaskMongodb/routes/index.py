c8787d7df5e862d2238b3216b882e98a544503a5
Mony SOEURN <soeurn.mony@gmail.com>
2020-06-02 21:27:46 +0700

    [TASK] implemented Create, Update and Delete of post

--- crudFlaskMongodb/routes/index.py
+++ crudFlaskMongodb/routes/index.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import Flask, render_template, url_for, flash, redirect, jsonify, request
+from flask import Flask, render_template, url_for, flash, redirect, jsonify, request, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from crudFlaskMongodb import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
--- crudFlaskMongodb/routes/post.py
+++ crudFlaskMongodb/routes/post.py
@@ -16,4 +16,45 @@ def new_post():
         flash('Your Post has been created!', 'success')
         return redirect(url_for('home'))
 
-    return render_template('posts/create_post.html', title= 'New Post', form=form)
+    return render_template('posts/create_post.html', title= 'New Post', form=form, legend='New Post')
+
+@app.route("/post/<post_id>")
+def post(post_id):
+    post = Post.objects().filter(id = post_id).first()
+    if post:
+        return render_template('posts/post_detail.html', title=post.title, post= post)
+    else:
+        return render_template('404.html', title = '404 Not Found')
+
+@app.route("/post/<post_id>/update", methods = ['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.objects().filter(id = post_id).first()
+    if post.author.id != current_user.id:
+        abort(403)
+    if post is None:
+        return render_template('404.html', title = '404 Not Found')
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        post.update(title=form.title.data, content=form.content.data)
+        flash('Your Post has been updated!.', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('posts/create_post.html', title= 'Update Post', form=form, legend='Update Post')
+
+@app.route("/post/<post_id>/delete", methods = ['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.objects().filter(id = post_id).first()
+    if post.author.id != current_user.id:
+        abort(403)
+    if post is None:
+        return render_template('404.html', title = '404 Not Found')
+    else:
+        post.delete()
+        flash('You have successfully deleted post!.', 'success')
+        return redirect(url_for('home'))
